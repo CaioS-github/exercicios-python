475080cb53f313c75a347b83984cdb610c639fbb
CaioS-github <77083015+CaioS-github@users.noreply.github.com>
2021-02-17 22:40:12 -0300

    17/02/2021
    Não fiz no dia, mas fiz um commit neste.

---
+++ exercicio022.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome completo aqui: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em letras maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+separado = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separado[0], len[0]))
