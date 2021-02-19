df1b715e74ccaf078d370b3f143a67a84f82f6b3
CaioS-github <77083015+CaioS-github@users.noreply.github.com>
2021-02-19 19:18:11 -0300

    atualizando

---
+++ exercicio021.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('darth-vader.wav')
+pygame.mixer.music.play
+paygame.event.wait()
+
+#ver depois
--- exercicio028.py
+++ exercicio028.py
@@ -1,7 +1,9 @@
 from random import randint
+from time import sleep
 print('Tente advinhar um número entre 0 e 5')
 num_dig = int(input('Digite aqui seu palpite: '))
 valor = randint(0, 5)
+sleep(3)
 print('O valor escolhido pelo computador foi {}.'.format(valor))
 if valor == num_dig:
     print('Acertou! Tá colando, é? Meus parabéns!')
