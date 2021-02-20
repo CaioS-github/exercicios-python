235527e37f1bb61a4f4da38db16791798a9fa2a9
CaioS-github <77083015+CaioS-github@users.noreply.github.com>
2021-02-20 20:45:48 -0300

    Create exercicio030.py

---
+++ exercicio030.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um número: '))
+resultado = num % 2     #Aqui no código eu peço para que ele me mostre o resto da divisão por 2
+if resultado == 1:
+    print('O número que você digitou é impar, meu caro.')
+else:
+    print('Este número que foi digitado é par, meu caro.')
