7e73d8a039271357d0980481bc9cad75835417eb
CaioS-github <77083015+CaioS-github@users.noreply.github.com>
2021-02-20 21:53:03 -0300

    Atualizando

--- exercicio031.py
+++ exercicio031.py
@@ -1,12 +1,12 @@
 print('=-=' * 10)
 print('CAIO TRANSPORTE LTDA')
 print('=-=' * 10)
-nome = input('Qual o seu nome? ')
-numero = float(input('Qual a distância que você percorrirá? R$'))
+nome = str(input('Qual o seu nome? '))
+numero = float(input('Qual a distância que você irá percorrer? R$'))
 if numero < 200:
     opcao1 = numero * 0.50
-    print('O valor pago será R${}.'.format(opcao1))
+    print('O valor pago será R${:.2f}.'.format(opcao1))
 else:
     opcao2 = numero * 0.45
-    print('Na promoção você pagará R${}'.format(opcao2))
-print('Agradecemos pela preferência, {}. Volte sempre.')
+    print('Na promoção você pagará R${:.2f}'.format(opcao2))
+print('Agradecemos pela preferência, {}. Volte sempre.'.format(nome))
