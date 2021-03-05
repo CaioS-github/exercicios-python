243a8a415b4b101b87753ac8a3ea2ded88ac2fc9
CaioS-github <77083015+CaioS-github@users.noreply.github.com>
2021-03-04 22:58:23 -0300

    04/03/2021

--- exercicio040.py
+++ exercicio040.py
@@ -1,9 +1,10 @@
+nome = str(input('Qual o nome do aluno? '))
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
 if media >= 7.0:
-    print('APROVADO')
+    print('{}, você está APROVADO com a média de {:.1f}.'.format(nome, media))
 elif media > 5.1 and media < 6.9:
-    print('RECUPERAÇÃO')
+    print('{}, você está de RECUPERAÇÃO, pois sua média foi de {}.'.format(nome, media))
 elif media < 5.0:
-    print('REPROVADO')
+    print('{}, você está REPROVADO e sua média foi de {}.'.format(nome, media))
